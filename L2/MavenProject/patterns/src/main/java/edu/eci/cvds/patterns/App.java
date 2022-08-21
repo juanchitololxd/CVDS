e676a77dadef4d3a5e1c8cc08df960fd58c1f8a9
Juan Fonseca <juanpablofonsecac5@gmail.com>
2022-08-21 18:05:50 -0500

    commit por si pablo se tira todo creando los componentes

---
+++ L2/MavenProject/patterns/src/main/java/edu/eci/cvds/patterns/App.java
@@ -0,0 +1,13 @@
+package edu.eci.cvds.patterns;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( String.format("Que mas %s que dice la raza manito", args[0]));
+    }
+}

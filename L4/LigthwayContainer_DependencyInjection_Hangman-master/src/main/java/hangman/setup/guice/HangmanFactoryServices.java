f27b9a91d140d936b4dc0683b83716271687935b
Juan Fonseca <juanpablofonsecac5@gmail.com>
2022-09-19 08:05:07 -0500

    prueba

--- L4/LigthwayContainer_DependencyInjection_Hangman-master/src/main/java/hangman/setup/guice/HangmanFactoryServices.java
+++ L4/LigthwayContainer_DependencyInjection_Hangman-master/src/main/java/hangman/setup/guice/HangmanFactoryServices.java
@@ -7,6 +7,9 @@ package hangman.setup.guice;
 
 import java.util.Dictionary;
 
+import hangman.model.BonusScore;
+import hangman.model.English;
+
 /**
  *
  * @author 2106913
@@ -16,7 +19,9 @@ import hangman.model.French;
 import hangman.model.GameScore;
 import hangman.model.Language;
 import hangman.model.OriginalScore;
+import hangman.model.PowerScore;
 import hangman.model.dictionary.HangmanDictionary;
+import hangman.model.dictionary.EnglishDictionaryDataSource;
 import hangman.model.dictionary.FrenchDictionaryDataSource;
 import hangman.view.HangmanColoridoPanel;
 import hangman.view.HangmanNoviolentoPanel;
@@ -26,10 +31,10 @@ public class HangmanFactoryServices extends com.google.inject.AbstractModule {
 
     @Override
     protected void configure() {
-    	bind(Language.class).to(French.class);
-    	bind(HangmanDictionary.class).to(FrenchDictionaryDataSource.class);
+    	bind(Language.class).to(English.class);
+    	bind(HangmanDictionary.class).to(EnglishDictionaryDataSource.class);
     	bind(HangmanPanel.class).to(HangmanColoridoPanel.class);
-    	bind(GameScore.class).to(OriginalScore.class);
+    	bind(GameScore.class).to(BonusScore.class);
     	
     }
 

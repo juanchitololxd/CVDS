3498f71f7bea8f07589b85ef2bd7e6b2feaece3f
Juan Fonseca <juanpablofonsecac5@gmail.com>
2022-09-21 16:02:18 -0500

    falta la ultima parte pero en la noche la hago xd

---
+++ L5 - copia/servlet/src/main/java/edu/eci/cvds/servlet/model/GuessBean.java
@@ -0,0 +1,78 @@
+package edu.eci.cvds.servlet.model;
+
+import java.util.List;
+
+import java.util.Random;
+
+import javax.faces.bean.ApplicationScoped;
+//import javax.faces.bean.ManagedBean;
+import javax.inject.Named;
+
+import java.util.ArrayList;
+import javax.faces.bean.ManagedBean;
+
+
+@ManagedBean(name = "guessBean")
+// @Named
+@ApplicationScoped
+public class GuessBean {
+    private int premioStart = 100000;
+    private int disminucion;
+    private int numeroGuess; 
+    private int premio;   
+    private List<Integer> intentos;
+    private String status = "Sin ganar";
+
+    public GuessBean(){
+        this.init();
+    }
+
+    private void init(){
+        this.premio = premioStart;
+        this.disminucion = 10000;
+        this.numeroGuess = (new Random()).nextInt(50);
+        this.intentos = new ArrayList<Integer>();
+        this.status = "Sin ganar";
+    }
+
+    
+    public int getNumeroGuess(){
+        return this.numeroGuess;
+    }
+
+
+    public int getTotalIntentos(){
+        return this.intentos.size();
+    }
+
+    public String getStatus(){
+        return this.status;
+    }
+
+    public int getPremio(){
+        return this.premio;
+    }
+    /**
+     * Verifica algo :v
+     * @param sIntento
+     * @return
+     */
+    public boolean guess(int intento){
+        boolean result = false;
+        if (this.numeroGuess == intento) result = true;
+        if(!result) disminuirPremio();
+        return result;
+    }
+
+    public void disminuirPremio(){
+        this.premio -= this.disminucion; 
+    }
+
+    /**
+     * Reinicia el juego
+     */
+    public void restart(){
+        this.init();
+    }
+
+}

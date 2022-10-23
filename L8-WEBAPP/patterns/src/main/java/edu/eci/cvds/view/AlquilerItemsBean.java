898d331a8f0927c44376dcf802a7001fe034daa5
SantiagoC <cardenas.santiago@hotmail.com>
2022-10-22 23:46:36 -0500

    alquilar items

--- L8-WEBAPP/patterns/src/main/java/edu/eci/cvds/view/AlquilerItemsBean.java
+++ L8-WEBAPP/patterns/src/main/java/edu/eci/cvds/view/AlquilerItemsBean.java
@@ -73,5 +73,44 @@ public class AlquilerItemsBean extends BasePageBean {
 	return this.cliente;
     }
 
+    /**
+     * Retorna una lista con la informacion de los items rentados por un cliente dado
+     * @param documento documento del cliente solicitado
+     * @return lista con la informacion de los items rentados por un cliente dado
+     */
+    public List<ItemRentado> consultarItemsCliente(Cliente cliente) throws ExcepcionServiciosAlquiler{
+        return serviciosAlquiler.consultarItemsCliente((int) cliente.getDocumento());
+    }
+
+    /**
+    * Registra un nuevo alquiler con el item y numero de dias especificados
+    * @param idItem identificador unico del item deseado
+    * @param numDays numero de dias por los cuales se solicita el item
+    */
+    public void registrarAlquiler(int idItem , int numDays) throws ExcepcionServiciosAlquiler {
+	try{
+        	Item item = serviciosAlquiler.consultarItem(idItem);
+        	serviciosAlquiler.registrarAlquilerCliente(new Date(System.currentTimeMillis()),cliente.getDocumento(),item,numDays);
+	}catch(Exception e){
+		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR,"Error ",e.getMessage()));
+	}
+    }
+
+    public void setCostoAlquiler(long costoAlquiler){
+	this.costoAlquiler = costoAlquiler;
+    }
+    
+    public long getCostoAlquiler(){
+	return costoAlquiler;
+    }
+
+    /**
+    * Consulta la multa asociada a un item dado
+    * @param idItem id del item a consultar
+    */
+    public long consultarMulta(int iditem) throws ExcepcionServiciosAlquiler {
+	return serviciosAlquiler.consultarMultaAlquiler(iditem, new Date(System.currentTimeMillis()));
+    }
+
 
 }
